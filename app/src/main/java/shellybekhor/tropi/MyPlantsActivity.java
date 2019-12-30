665ede6c17ccd2b01e97d0f98b7b3548666830c6
libimeshorer <li.meshorer@mail.huji.ac.il>
2019-12-30 18:17:54 +0200

    adding the horizontal scroll to "my plants"

---
+++ app/src/main/java/shellybekhor/tropi/MyPlantsActivity.java
@@ -0,0 +1,36 @@
+package shellybekhor.tropi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+// https://www.youtube.com/watch?v=hl0AcuplFwE
+
+public class MyPlantsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_plants);
+
+        LinearLayout socculentShelf = findViewById(R.id.SocculentShelf);
+        LayoutInflater inflater = LayoutInflater.from(this);
+
+        for (int i = 0 ; i < 3; i++) {
+            View view =inflater.inflate(R.layout.socculent_item, socculentShelf, false);
+            TextView textView = view.findViewById(R.id.socculentName);
+            textView.setText("item" + i);
+
+            ImageView imageView = view.findViewById(R.id.socculentImage);
+            imageView.setImageResource(R.mipmap.ic_launcher);
+
+            socculentShelf.addView(view);
+        }
+    }
+}
