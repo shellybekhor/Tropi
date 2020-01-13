2e7d3dede91e01547d1a03737b2f9e2d4438bf25
almogargaman <a.almogargaman@gmail.com>
2020-01-13 14:13:58 +0200

    design

---
+++ app/src/main/java/shellybekhor/tropi/PlantInfo.java
@@ -0,0 +1,14 @@
+package shellybekhor.tropi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlantInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plant_info);
+    }
+}
